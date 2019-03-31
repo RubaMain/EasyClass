27c05537d52e3faf0fbc03ed17ac7f24f86ffe0e
Rubén <rubenezkobar@hotmail.com>
2019-03-31 02:38:32 -0600

    Project creation

---
+++ EasyClass.API/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EasyClass.API.WebApiApplication" Language="C#" %>
---
+++ EasyClass.Backend/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace EasyClass.Backend
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
