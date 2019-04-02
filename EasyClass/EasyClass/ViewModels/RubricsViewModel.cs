a2d3fab4645ed2dbae669c05775d5249f8a25cb6
Rubén <rubenezkobar@hotmail.com>
2019-04-02 05:45:37 -0600

    Refreshing ListView

--- EasyClass/EasyClass/ViewModels/RubricsViewModel.cs
+++ EasyClass/EasyClass/ViewModels/RubricsViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using EasyClass.Common.Models;
 using EasyClass.Services;
 using EasyClass.ViewModels;
+using GalaSoft.MvvmLight.Command;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -12,6 +13,8 @@ public class RubricsViewModel : BaseViewModel
     //Variables e Instancias.
     private ApiService apiService;
 
+    private bool isRefreshing;
+
     private ObservableCollection<Rubric> rubrics;
 
     //Colección de Rúbricas.
@@ -21,6 +24,13 @@ public class RubricsViewModel : BaseViewModel
         set { this.SetValue(ref this.rubrics, value); }
     }
 
+    //To Refresh the RubricListView in the Application.
+    public bool IsRefreshing
+    {
+        get { return this.isRefreshing; }
+        set { this.SetValue(ref this.isRefreshing, value); }
+    }
+
 
     public RubricsViewModel()
     {
@@ -28,32 +38,36 @@ public class RubricsViewModel : BaseViewModel
         this.LoadRubrics();
     }
 
+    //To show The title and the description of the rubric
     public void HideOrShowRubric(Rubric rubric)
     {
         rubric.IsVisible = true;
     }
 
-    //Metodo asincrono para refrescar la consulta de
-    //una lista de rúbricas desde la app sin salir de ella.
+    //Method async to refresh a query of
+    //a list of rubrics from the application without leaving it.
     private async void LoadRubrics()
     {
+        this.IsRefreshing = true;
         var response = await this.apiService.GetList<Rubric>("https://easyclassapi.azurewebsites.net", "/api", "/Rubrics");
         if (!response.IsSuccess)
         {
+            this.IsRefreshing = false;
             await Application.Current.MainPage.DisplayAlert("Error", response.Message, "Accept");
             return;
         }
 
         var list = (List<Rubric>)response.Result;
         this.Rubrics = new ObservableCollection<Rubric>(list);
+        this.IsRefreshing = false;
     }
 
     //Comando usado para ejecutar nuevamente el método que carga la lista de rúbricas en la aplicación.
-    /*public ICommand RefreshCommand
+    public ICommand RefreshCommand
     {
         get
         {
             return new RelayCommand(LoadRubrics);
         }
-    }*/
+    }
 }
